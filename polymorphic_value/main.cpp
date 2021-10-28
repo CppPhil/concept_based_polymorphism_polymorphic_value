206e06c9e068aece6b7cf50e8d4d80e20d369056
Phillip Schackier <CppPhil@users.noreply.github.com>
2021-10-28 17:50:46 +0200

    Update

--- polymorphic_value/main.cpp
+++ polymorphic_value/main.cpp
@@ -1,9 +1,83 @@
 ﻿#include <iostream>
+#include <utility>
 
-using namespace std;
+#include <polymorphic_value.h>
+
+class Vehicle
+{
+private:
+  class Interface
+  {
+  public:
+    virtual ~Interface() = default;
+    virtual void drive() = 0;
+  };
+
+  template <typename ImplementationType>
+  class Delegator : public Interface
+  {
+  public:
+    explicit Delegator(const ImplementationType& implementation)
+      : m_implementation{implementation}
+    {
+    }
+
+    explicit Delegator(ImplementationType&& implementation)
+      : m_implementation{std::move(implementation)}
+    {
+    }
+
+    void drive() override
+    {
+      m_implementation.drive();
+    }
+
+  private:
+    ImplementationType m_implementation;
+  };
+
+public:
+  template <typename ImplementationType>
+  explicit Vehicle(ImplementationType&& implementation)
+    : m_value{
+        std::in_place_type<Delegator<ImplementationType>>,
+        Delegator<ImplementationType>{std::forward<ImplementationType>(implementation)}
+  }
+  {
+  }
+
+  void drive()
+  {
+    m_value->drive();
+  }
+
+private:
+  isocpp_p0201::polymorphic_value<Interface> m_value;
+};
+
+class Car
+{
+public:
+  void drive()
+  {
+    std::cout << "Vroom, vroom. I'm a car!\n";
+  }
+};
+
+class Truck
+{
+public:
+  void drive()
+  {
+    std::cout << "VROOOM. I'm a truck!\n";
+  }
+};
 
 int main()
 {
-  cout << "Hello CMake." << endl;
-  return 0;
+  Vehicle vehicle{Car{}};
+  vehicle.drive();
+
+  vehicle = Vehicle{Truck{}};
+  vehicle.drive();
 }
